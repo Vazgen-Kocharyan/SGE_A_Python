11dd45c4dc8004b5a57433a6bd448c167be5c9f7
Vazgen <2024_vazgen.kocharyan@iticbcn.cat>
2024-11-19 15:16:12 +0100

    branch activitat 3 vaciada

--- exercici1.py
+++
@@ -1,9 +0,0 @@
-num1 = int(input("Numero 1: "))
-num2 = int(input("Numero 2: "))
-
-if num1 > num2:
-	print(f'{num1} es mes gran que {num2}')
-elif num2 > num1:
-	print(f'{num2} es mes gran que {num1}')
-else:
-	print("Son iguals")
